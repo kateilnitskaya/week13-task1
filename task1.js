0a6d71909a41a2980b22828bbf9455659d21482a
Kate Ilnitskaya <kateilnitskaya@gmail.com>
2022-02-23 19:07:59 +0500

    edit task1

---
+++ task1.js
@@ -0,0 +1,13 @@
+/*let message_to = `Привет, ${name}`;
+
+alert(message_to);*/
+
+
+/*function showMessageFromUser(name, message) {
+    alert(`${name}: ${message}`);
+}
+showMessageFromUser('Kate', 'hello!');*/
+
+
+/*let name = prompt('Как тебя зовут?');
+alert(`Привет, ${name}!`);*/
